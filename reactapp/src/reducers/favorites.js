28ddd6675c9f78191985f5f98cc002110a114806
Stéphanie Bézard <steph.bezard@gmail.com>
2021-03-23 14:14:33 +0100

    naze

--- reactapp/src/reducers/favorites.js
+++ reactapp/src/reducers/favorites.js
@@ -3,39 +3,40 @@ export default function (wishList = [], action) {
     let wishListCopy = [...wishList];
     wishListCopy.push(action.articleLiked);
 
-    let findArticle = false;
+    // let findArticle = false;
 
-    for (let i = 0; i < wishListCopy.length; i++) {
-      if (wishListCopy[i].article == action.articleLiked.article) {
-        findArticle = true;
-      }
-    }
+    // for (let i = 0; i < wishListCopy.length; i++) {
+    //   if (wishListCopy[i].article == action.articleLiked.article) {
+    //     findArticle = true;
+    //   }
+    // }
 
-    if (!findArticle) {
-      wishListCopy.push(action.articleLiked);
-    }
+    // if (!findArticle) {
+    //   wishListCopy.push(action.articleLiked);
+    // }
 
     return wishListCopy;
+    
   } else if (action.type === "deleteArticle") {
-    // var wishListCopy = wishList.filter(
-    //   (element) => element.article != action.articleDisliked.article
-    // );
+    var wishListCopy = wishList.filter(
+      (element) => element._id != action.articleDisliked.id
+    );
     
     // console.log("00", wishList);
     // console.log("02", wishListCopy);
-    
-        let wishListCopy = [...wishList];
-        let position = null;
 
-        for (let i = 0; i < wishListCopy.length; i++) {
-          if (wishListCopy[i].article == action.articleDisliked.article) {
-            position = i;
-          }
-        }
+        // let wishListCopy = [...wishList];
+        // let position = null;
 
-        if (position !== null) {
-          wishListCopy.splice(position, 1);
-        }
+        // for (let i = 0; i < wishListCopy.length; i++) {
+        //   if (wishListCopy[i].article == action.articleDisliked.article) {
+        //     position = i;
+        //   }
+        // }
+
+        // if (position !== null) {
+        //   wishListCopy.splice(position, 1);
+        // }
 
         return wishListCopy;
   } else {
