1b39de904dda1fdaef001e8fff46b73b4ad6b11b
Stéphanie Bézard <steph.bezard@gmail.com>
2021-04-11 21:59:07 +0200

    jest ok

--- routes/index.test.js
+++ routes/index.test.js
@@ -3,83 +3,79 @@ var request = require("supertest")
 const UserModel = require("../models/users")
 const { connection, Mongoose } = require("mongoose")
 
-describe('Tests routes plantes', () => {
+describe('Tests routes plantes ET user', () => {
 
-    router.post('/products', (req, res) => {
-    let name = req.body.name;
+    // router.post('/products', (req, res) => {
+    // let name = req.body.name;
 
-    res.json({ name: name });
-    });
+    // res.json({ name: name });
+    // });
 
-    test("compte inconnu", async (done) => {
-        await request(app).get('/products')
-          .send({ 
-              name: 'cactus',
-           })
-          .expect(200)
-          .expect({ 
-              error: ['email inexistant']
-           })
-           done();
-        })
+    // test("compte inconnu", async (done) => {
+    //     await request(app).get('/products')
+    //       .send({ 
+    //           name: 'cactus',
+    //        })
+    //       .expect(200)
+    //       .expect({ 
+    //           error: ['email inexistant']
+    //        })
+    //        done();
+    //     })
 
-
-   
    
 
     // beforeAll(() => {
     //     connection();
     // })
 
-    // beforeEach(async () => {
-    //     await UserModel.deleteMany()
+    beforeEach(async () => {
+        await UserModel.deleteMany()
 
-    //     var userTest = new UserModel({
-    //         firstname: 'jean',
-    //         lastname: 'paul',
-    //         email: 'adresse@domaine.fr',
-    //         password: 'azerty',
-    //         token: 'jscjshoer',
-    //         status: 'client'
-    //     });
+        var userTest = new UserModel({
+            firstname: 'jean',
+            lastname: 'paul',
+            email: 'adresse@domaine.fr',
+            password: 'azerty',
+            token: 'jscjshoer',
+            status: 'client'
+        });
 
-    //     return userTest.save()
-    // })
+        return userTest.save()
+    })
 
-    // afterAll((done) => {
-    //     Mongoose.disconnect(done);
-    // })
+    afterAll((done) => {
+        Mongoose.disconnect(done);
+    })
 
-    // test("compte inconnu", async (done) => {
-    //     await request(app).post('/sign-in')
-    //       .send({ 
-    //           'email': 'adresse@domaine.fr',
-    //            'password': 'test'
-    //        })
-    //       .expect(200)
-    //       .expect({ 
-    //           result: false,
-    //           data: {token: null},
-    //           error: ['email inexistant']
-    //        })
+    test("compte inconnu", async (done) => {
+        await request(app).post('/sign-in')
+          .send({ 
+              'email': 'adresse@domaine.fr',
+               'password': 'test'
+           });
+          expect({ 
+              result: false,
+              data: {token: null},
+              error: ['email inexistant']
+           })
        
-    //     done();
-    //    });
+        done();
+       });
        
-    //    test("mauvais mot de passe", async (done) => {
-    //        await request(app).post('/sign-in')
-    //          .send({ 
-    //              'email': 'adresse@domaine.fr',
-    //               'password': 'test'
-    //           })
-    //          .expect(200)
-    //          .expect({ 
-    //              result: false,
-    //              data: {token: null},
-    //              error: ['mauvais mot de passe']
-    //           })
+       test("mauvais mot de passe", async (done) => {
+           await request(app).post('/sign-in')
+             .send({ 
+                 'email': 'adresse@domaine.fr',
+                  'password': 'test'
+              });
+-             expect({ 
+                 result: false,
+                 data: {token: null},
+                 error: ['mauvais mot de passe']
+              })
           
-    //        done();
-    //       });
+           done();
+          });
   })
 
